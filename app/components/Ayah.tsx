f4d1526638ad67e385e6c43c836620c71f6a28fa
mdchad <irsyad.muhd@gmail.com>
2024-04-18 00:02:56 +0800

    add verse number

--- app/components/Ayah.tsx
+++ app/components/Ayah.tsx
@@ -1,8 +1,19 @@
 "use client";
 
+function getUnicodeCharacter(n) {
+  const baseHex = 'FC00';
+  const baseDecimal = parseInt(baseHex, 16);
+  const targetDecimal = baseDecimal + (n - 1);
+  const targetHex = targetDecimal.toString(16).toUpperCase();
+  const unicodeCharacter = String.fromCodePoint(parseInt(targetHex, 16));
+  return unicodeCharacter;
+}
+
+
 const Ayah = ({ data }: any) => {
+  console.log(data.verse)
   return (
-    <p lang="ar" dir="rtl" className="text-justify font-arabic text-3xl leading-loose">{data.verse.text_imlaei}</p>
+    <p lang="ar" dir="rtl" className="font-arabic text-3xl leading-loose">{data.verse.text_imlaei}<span>{getUnicodeCharacter(data.verse.verse_number)}</span></p>
   );
 };
 
